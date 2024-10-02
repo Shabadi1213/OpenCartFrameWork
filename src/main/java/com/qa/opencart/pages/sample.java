716d10d917e79ccb18743a3c588a9e44c111cec0
Shabadi1213 <tejas82in@gmail.com>
2024-10-02 15:37:39 +0530

    Update sample.java

--- src/main/java/com/qa/opencart/pages/sample.java
+++ src/main/java/com/qa/opencart/pages/sample.java
@@ -1,1 +1,15 @@
+package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
+public class SamplePage {
+	
+	int i =10;
+
+	By locator = By.id("sample-id");
+	
+	public void getPage() {
+		System.out.println("SamplePage test");
+	}
+
+}
