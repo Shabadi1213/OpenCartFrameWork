74d13c021d149c5b6e3aac1feb3b4dea9ae38e7a
Rshabadi <tejas82in@gmail.com>
2024-10-05 18:07:31 +0530

    Added F1 and F2 Feature

--- src/main/java/Feature/feature.java
+++ src/main/java/Feature/feature.java
@@ -1,5 +1,15 @@
 package Feature;
 
 public class feature {
+	
+	
+	public void test() {
+		
+		System.out.println("Feature F1");
+	}
 
+	public void test2() {
+		
+		System.out.println("Feature F2");
+	}
 }
